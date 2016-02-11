08ac7fe624345f5dde20eb09521d0d3d6355fe90
archeryu <s0962593@mail.ncyu.edu.tw>
2016-02-11 16:35:31 +0800

    add for loop

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -7,7 +7,11 @@ public class main {
 		int test01;
 		int test02;
 		int test03;
-		String string01;
+		String string01 = "Hello";
+		
+		for(int i=0;i>2;i++){
+			System.out.println(string01+i);//add forloop
+		}
 		
 		
 	}
